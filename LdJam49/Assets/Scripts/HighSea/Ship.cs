d12bbeadf1ca827c51fd10790091b1dab4708326
Rentem <salasam@gmx.net>
2021-10-02 12:11:54 +0200

    HighSea testing

---
+++ LdJam49/Assets/Scripts/HighSea/Ship.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+    public RectTransform ShipTransform;
+    private Vector3 RotationAxis;
+    void Start()
+    {
+        RotationAxis = new Vector3(0, 0, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ShipTransform.RotateAround(RotationAxis, 5);
+    }
+}
