eb6ddef5e990ee8a438d8d834c371cc3a4afaf63
XpArKeR <xparker@gmx.net>
2021-10-02 10:50:20 +0200

    Added Initial MainMenu Scene and Button prefab

---
+++ LdJam49/Assets/Scripts/MainMenu/MainMenuScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public GameObject MainMenuContainer;
+    public GameObject OptionsMenuContainer;
+    public GameObject CreditsContainer;
+    public Text VersionText;
+
+    public GameObject QuitButton;
+    public GameObject BackButton;
+
+    public AudioSource AudioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
