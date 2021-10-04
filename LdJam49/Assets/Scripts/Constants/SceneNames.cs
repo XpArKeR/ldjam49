3122488011152155acecfe3878fe4af9e786204f
XpArKeR <xparker@gmx.net>
2021-10-04 16:10:01 +0200

    Set name, updated port to use new HighSea2 Scene

--- LdJam49/Assets/Scripts/Constants/SceneNames.cs
+++ LdJam49/Assets/Scripts/Constants/SceneNames.cs
@@ -6,6 +6,7 @@ namespace Assets.Scripts.Constants
     {
         public const String MainMenu = "MainMenuScene";
         public const String HighSea = "HighSea";
+        public const String HighSea2 = "HighSea2";
         public const String Port = "Port";
         public const String GameOver = "GameOverScene";
     }
